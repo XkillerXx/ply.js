f2b83eebd96ca5074652755a2146e46362c98e47
XkillerXx <38698562+XkillerXx@users.noreply.github.com>
2019-02-10 19:42:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,37 +13,31 @@ KiNg66S.on('message', message => {
   if (message.author.id !== "350968179927023619") return;
 
   
-  if (message.content.startsWith(PREFIX + 'setwatch')) {
+  if (message.content.startsWith(PREFIX + 'wt')) {
   KiNg66S.user.setActivity(argresult, {type: 'WATCHING'})
      console.log('test' + argresult);
     message.channel.sendMessage(`Watch Now: **${argresult}`)
 } 
 
  
-  if (message.content.startsWith(PREFIX + 'setlis')) {
+  if (message.content.startsWith(PREFIX + 'ls')) {
   KiNg66S.user.setActivity(argresult, {type: 'LISTENING'})
      console.log('test' + argresult);
     message.channel.sendMessage(`LISTENING Now: **${argresult}`)
 } 
 
 
-if (message.content.startsWith(PREFIX + 'setname')) {
-  KiNg66S.user.setUsername(argresult).then
-      message.channel.sendMessage(`Username Changed To **${argresult}**`)
-  return message.reply("You Can change the username 2 times per hour");
-} 
 
-if (message.content.startsWith(PREFIX + 'setavatar')) {
-  KiNg66S.user.setAvatar(argresult);
-   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
-}
 
-if (message.content.startsWith(PREFIX + 'setstream')) {
+
+
+
+if (message.content.startsWith(PREFIX + 'st')) {
   KiNg66S.user.setGame(argresult, "https://www.twitch.tv/KiNg66S");
      console.log('test' + argresult);
     message.channel.sendMessage(`Streaming: **${argresult}`)
 } 
-if (message.content.startsWith(PREFIX + 'setplay')) {
+if (message.content.startsWith(PREFIX + 'ply')) {
   KiNg66S.user.setGame(argresult);
      console.log('test' + argresult);
     message.channel.sendMessage(`Playing: **${argresult}`)
